6b243759fb34db92d85de00aaf37d73a49231734
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-04-19 08:33:43 +0530

    gta4l: Add Lunaris maintainer

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/gta4l/gta4l-vendor.mk)
 
+# Lunaris Maintainer prop
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.lunaris.maintainer=Shipu
+
 # Local overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
